9f88bf2d34f8cc7721392628e473f3ebac3d85ce
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-08-16 23:08:27 -0400

    tried moving generate function in other file.

--- questions.js
+++ questions.js
@@ -60,8 +60,65 @@ const readmeQuestions = [
             "License 4",
         ]
      },
-]
+];
+
+const populateReadMe = (projectTitle, projectLink, description, installationInstructions, usage, developerName, email, gitHubUsername, contributing, tests, license) => {
+    return `# ${projectTitle}
+
+
+    <!-- TABLE OF CONTENTS -->
+    ## Table of Contents
+    * [About the Project](#about-the-project)
+        * [Finished Project Link](#finished-project)
+        * [Project Screenshot](#project-screenshot)
+    * [Installation Instructions](#installation-instructions)
+    * [Usage](#usage)
+    * [Questions](#questions)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [License](#license)
+    
+    ## About The Project
+    
+    ${description}
+    
+    
+    ### Finished Project
+    Project Link: [${projectTitle}](${projectLink})
+    
+    
+    ### Project Screenshot
+    
+    
+    
+    ## Installation Instructions
+    
+    ${installationInstructions}
+    
+    ## Usage
+    
+    ${usage}
+    
+    ## Questions
+    
+    ### ${developerName}
+    * [${email}](${email})
+    * [https://github.com/${gitHubUsername}](https://github.com/${gitHubUsername})
+    
+    ## Contributing
+    
+    ${contributing}
+    
+    ## Tests
+    
+    ${tests}
+    
+    # License
+    
+    ${license}`
+}
 
 module.exports = {
-    readmeQuestions: readmeQuestions
+    readmeQuestions,
+    populateReadMe
 };
