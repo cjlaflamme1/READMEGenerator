70ad7133fbe072a9657c0d591a30038e96c01212
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-08-18 21:44:10 -0400

    cleaned up the code

--- index.js
+++ index.js
@@ -4,26 +4,23 @@ const { readmeQuestions, populateReadMe } = require("./questions");
 const licenses = require('./licenses');
 
 
-inquirer.prompt(readmeQuestions).then(function ({ projectTitle, projectLink, description, installationInstructions, usage, developerName, email, gitHubUsername, contributing, tests, license }) {
-    let selectedLicense = '';
-    let selectedBadge = '';
-    licenses.licenses.forEach(function (item) {
-        if (item.title === license.toString()) {
-            //    console.log(license);
-            //    console.log(item);
-            selectedLicense = item.text;
-            selectedBadge = item.badge;
-        }
-    })
+// This is some code Mike the TA shared with me to help me understand Promises.
+// const writeFileAsync = (fileName, data) => {
+//     return fs.writeFile(fileName, data, (err) => {
+//         if (err) return Promise.reject(err);
+//         return Promise.resolve();
+//     })
+// };
 
+inquirer.prompt(readmeQuestions).then(function ({ projectTitle, projectLink, description, installationInstructions, usage, developerName, email, gitHubUsername, contributing, tests, license }) {
+    const { text: selectedLicense, badge: selectedBadge } = licenses.licenses.find((item) => item.title === license);
     const readME = populateReadMe(projectTitle, projectLink, description, installationInstructions, usage, developerName, email, gitHubUsername, contributing, tests, selectedLicense, selectedBadge);
     console.log(readME);
-    try {
-        fs.writeFile("newREADME.md", readME, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-        })
-    } catch (err) {
-        console.log(err);
-    }
-})
+    fs.writeFile("newREADME.md", readME, (err) => {
+        if (err) console.log(err);
+        console.log('The file has been saved!');
+    });
+    // Continued code from Mike.
+    // return writeFileAsync("newREADME.md", readME);
+    // I didn't use this code because I thought I had a better understanding of what was represented above.
+}).catch((err) => console.log(err));
